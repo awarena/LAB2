01528189a8871e8d9acf38b7118b98d751379d53
awarena <76248192+awarena@users.noreply.github.com>
2022-09-14 19:36:42 -0400

    modify the response url

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function helloWorld(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 };
-app.use(helloWorld);
+app.use("/hello", helloWorld);
+app.use("/goodbye", helloWorld);
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
