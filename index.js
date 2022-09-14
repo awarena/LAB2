29bf12e2392e942099b9cf8596c59a48545bb4b6
awarena <76248192+awarena@users.noreply.github.com>
2022-09-14 19:38:50 -0400

    add function for the "/goodbye" url

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ function helloWorld(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 };
+function bye(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Bye World');
+};
 app.use("/hello", helloWorld);
-app.use("/goodbye", helloWorld);
+app.use("/goodbye", bye);
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
